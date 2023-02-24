457903f3ec49e7821ad39eaa3bfa08bc0050feaa
Kapelu <ddaniel_calderon@hotmail.com>
2023-02-23 22:19:17 -0300

    upDate 23/02/23

--- src/routes/Portfolio.js
+++ src/routes/Portfolio.js
@@ -6,14 +6,22 @@ export default function Portfolio() {
 		<>
 			<Nav />
 			<div className='container-portfolio'>
-				<div className='imagen'>
-					<img src='../img/profile.jpg' alt='' />
-				</div>
 				<div className='nombre'>
-					<p>Portfolio</p>
 					<h1>Daniel Calderon</h1>
 				</div>
 				<div className='datos'>
+					<br />
+					<div className='presentacion'>
+						<p>Gracias!!! por haber llegado hasta esta sección.</p>
+						<p>
+							Aqui estan varios de mis trabajos, donde eh aplicado
+							mis conocimientos como developer full stack.Junto a
+							la descripción agregare las tecnologias usadas en
+							cada proyecto.
+						</p>
+					</div>
+					<div className='container-slider'>
+					</div>
 				</div>
 			</div>
 		</>
