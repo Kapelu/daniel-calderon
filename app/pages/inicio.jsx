2b2ae41822c947406a71b679aa9963c600ccca6e
kapelu <ddaniel_calderon@hotmail.com>
2023-11-27 10:53:10 -0300

    UpDate

--- app/pages/inicio.jsx
+++ app/pages/inicio.jsx
@@ -6,9 +6,11 @@ export default function Inicio() {
 	return (
 		<>
 			<article
-				className='inicio-hero-image' /*style={{padding: ' 2rem 1rem'}}*/
-			>
-				<section className='inicio-hero-image-opacity'>
+				className='inicio-hero-image'>
+				<section
+					className='inicio-hero-image-opacity'
+					style={{padding: ' 2rem 1rem'}}
+				>
 					<h1 className='inicio-title'>Daniel Calderón</h1>
 					<h5 className='inicio-subtitle'>
 						Desarrollador Full Stack
@@ -22,29 +24,41 @@ export default function Inicio() {
 					/>
 				</section>
 				<section className='inicio-hero-image-opacity text-left text-lg-center'>
-					<p>
-						Soy un desarrollador <i>web</i> autodidacta, durante los
-						ultimos 3 años he dedicado gran parte de mi tiempo a
-						adquirir habilidades y conocimientos en el campo del
-						desarrollo <i>web</i>.
-					</p>
-					<p>
-						Soy una persona dedicada y comprometida con mi trabajo,
-						y estoy dispuesto a aprender y adquirir nuevos
-						conocimientos para mejorar mis habilidades como
-						programador. Estoy más orientado al <i>Backend</i>, pero
-						también hago <i>Frontend</i>. Me gusta enfrentar nuevos
-						desafíos y encontrar soluciones creativas a los
-						problemas que se presentan.
-					</p>
-                    
-					<p>
-						Estoy emocionado de tener la oportunidad de unirme a un
-						equipo y contribuir al crecimiento de una empresa. Creo
-						que esta va a ser una oportunidad ideal para seguir
-						creciendo y aprendiendo, y estoy dispuesto a trabajar
-						arduamente para alcanzar los objetivos de la empresa.
-					</p>
+					<div className='columnas container section'>
+                        <h2 className="section-title">¿Quien soy?</h2>
+						<div className='columna'>
+							<p>
+								Soy un desarrollador <i>web</i> autodidacta,
+								durante los ultimos 3 años he dedicado gran
+								parte de mi tiempo a adquirir habilidades y
+								conocimientos en el campo del desarrollo{' '}
+								<i>web</i>.
+							</p>
+						</div>
+						<div className='columna'>
+							<p>
+								Soy una persona dedicada y comprometida con mi
+								trabajo, y estoy dispuesto a aprender y adquirir
+								nuevos conocimientos para mejorar mis
+								habilidades como programador. Estoy más
+								orientado al <i>Backend</i>, pero también hago{' '}
+								<i>Frontend</i>. Me gusta enfrentar nuevos
+								desafíos y encontrar soluciones creativas a los
+								problemas que se presentan.
+							</p>
+						</div>
+						<div className='columna'>
+							<p>
+								Estoy emocionado de tener la oportunidad de
+								unirme a un equipo y contribuir al crecimiento
+								de una empresa. Creo que esta va a ser una
+								oportunidad ideal para seguir creciendo y
+								aprendiendo, y estoy dispuesto a trabajar
+								arduamente para alcanzar los objetivos de la
+								empresa.
+							</p>
+						</div>
+					</div>
 				</section>
 				<section className='inicio-hero-image-opacity'>
 					<h2 className='about__title'>Sobre mí</h2>
