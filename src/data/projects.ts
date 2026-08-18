ec4a981f880675cc351de2c62d95f06c6ce7a891
kapelu <ddaniel_calderon@hotmail.com>
2026-08-18 02:39:00 -0300

    ✔ Update 18-08-2026

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -15,7 +15,7 @@ export const projects = [
       'Un sistema administrativo de transporte. En estado de construcción, totalmente escalable.',
     image: '/portfolio/login.png',
     technologies: ['NextJS', 'TypeScript', 'Tailwind'],
-    demo: 'https://login-kapelu.vercel.app/',
+    demo: 'https://transporte-kapelu.vercel.app/',
   },
   {
     id: 'trabajo-blog-personal',
